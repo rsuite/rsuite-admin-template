e3b618460647ce5e382823b986bd32e22835e8f0
simonguo <simonguo.2009@gmail.com>
2022-08-17 23:14:27 +0800

    chore: Deploy to github pages

--- gh-pages.js
+++ gh-pages.js
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, './dist'), function(err) {
+ghpages.publish(path.join(__dirname, './assets'), function (err) {
   console.log(err);
 });
--- src/tracker.ts
+++
@@ -1,7 +0,0 @@
-export const pageview = title => {
-  window._ha &&
-    _ha('send', 'pageview', {
-      title: title || document.title,
-      url: document.location.href
-    });
-};
