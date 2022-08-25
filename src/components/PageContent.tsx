4ff2ecaee6265e1f098646abdcbbcdb08145c8e8
simonguo <simonguo.2009@gmail.com>
2022-08-25 17:34:44 +0800

    feat: add copyright

--- src/components/PageContent.tsx
+++ src/components/PageContent.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Panel } from 'rsuite';
+import Copyright from '@/components/Copyright';
 
 const PageContent = props => {
-  return <Panel style={{ background: '#fff' }} {...props} />;
+  return (
+    <>
+      <Panel style={{ background: '#fff' }} {...props} />
+      <Copyright />
+    </>
+  );
 };
 
 export default PageContent;
