46aab057316217c0d990098939887b305048a8fa
simonguo <simonguo.2009@gmail.com>
2022-08-25 18:03:10 +0800

    feat: change logo

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AdvancedAnalyticsIcon from '@rsuite/icons/AdvancedAnalytics';
+import Logo from './Logo';
+import { Stack } from 'rsuite';
 
 const Brand = props => {
   return (
-    <div className="brand" {...props}>
+    <Stack className="brand" {...props}>
+      <Logo height={26} style={{ marginTop: 6 }} />
       <Link to="/">
-        <AdvancedAnalyticsIcon style={{ fontSize: 22 }} />
-        <span style={{ marginLeft: 16 }}>Admin Template</span>
+        <span style={{ marginLeft: 14 }}>Admin Template</span>
       </Link>
-    </div>
+    </Stack>
   );
 };
 
